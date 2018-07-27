e038f1c6269bf10507c23e7d05fe509ae14d9da8
cferdinandi <chris@gomakethings.com>
2018-07-27 12:14:46 -0400

    Inlined all the thigns

--- themes/guides/src/js/main/zzz_init.js
+++ themes/guides/src/js/main/zzz_init.js
@@ -22,5 +22,8 @@ if (document.querySelector('#mailchimp-form')) {
 	});
 }
 
+// API for testimonials, CTAs, and sale pricing
+api();
+
 // Pricing parity
 pricingParity('https://gomakethings.com/checkout/wp-json/gmt-pricing-parity/v1/discount/', '<div class="container container-large"><img width="100" style="float:left;margin: 0 16px 16px 0;" src="https://flagpedia.net/data/flags/normal/{{iso}}.png"><p class="text-small no-margin-bottom">Hi! Looks like you\'re from <strong>{{country}}</strong>, where my <strong>Vanilla JS Pocket Guides</strong> might be a bit expensive. You can use the code <strong>{{code}}</strong> at checkout to take <strong>{{amount}}</strong> off any guide, course, or bundle. Cheers!</p></div>');
