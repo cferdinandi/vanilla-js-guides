540a223401711e3db8951d6f7fd48a9ca0dbefda
cferdinandi <chris@gomakethings.com>
2020-07-30 11:55:43 -0400

    Updated JS

---
+++ themes/gmt/static/js/swInit.js
@@ -0,0 +1,17 @@
+/*! guides v2.0.0 | (c) 2020 Chris Ferdinandi | MIT License | http://github.com/cferdinandi/vanilla-js-guides */
+(function () {
+	'use strict';
+
+	// Initialize the service worker
+	if (navigator && navigator.serviceWorker) {
+		navigator.serviceWorker.register('/sw.js');
+	}
+
+	// Cleanup old cache on page load
+	if (navigator.serviceWorker.controller) {
+		window.addEventListener('load', function () {
+			navigator.serviceWorker.controller.postMessage('cleanUp');
+		});
+	}
+
+}());
