d1e885f00f56838c309e870d096eed9a89a16179
cferdinandi <chris@gomakethings.com>
2020-07-30 14:18:19 -0400

    Updated service worker

--- themes/gmt/static/sw.js
+++ themes/gmt/static/sw.js
@@ -15,14 +15,14 @@ var limits = {
 
 // Font files
 var fontFiles = [
-	'https://gomakethings.com/fonts/pt-serif-v11-latin-regular.woff',
-	'https://gomakethings.com/fonts/pt-serif-v11-latin-regular.woff2',
-	'https://gomakethings.com/fonts/pt-serif-v11-latin-italic.woff',
-	'https://gomakethings.com/fonts/pt-serif-v11-latin-italic.woff2',
-	'https://gomakethings.com/fonts/pt-serif-v11-latin-700.woff',
-	'https://gomakethings.com/fonts/pt-serif-v11-latin-700.woff2',
-	'https://gomakethings.com/fonts/pt-serif-v11-latin-700italic.woff',
-	'https://gomakethings.com/fonts/pt-serif-v11-latin-700italic.woff2'
+	'https://cdn.gomakethings.com/fonts/pt-serif-v11-latin-regular.woff',
+	'https://cdn.gomakethings.com/fonts/pt-serif-v11-latin-regular.woff2',
+	'https://cdn.gomakethings.com/fonts/pt-serif-v11-latin-italic.woff',
+	'https://cdn.gomakethings.com/fonts/pt-serif-v11-latin-italic.woff2',
+	'https://cdn.gomakethings.com/fonts/pt-serif-v11-latin-700.woff',
+	'https://cdn.gomakethings.com/fonts/pt-serif-v11-latin-700.woff2',
+	'https://cdn.gomakethings.com/fonts/pt-serif-v11-latin-700italic.woff',
+	'https://cdn.gomakethings.com/fonts/pt-serif-v11-latin-700italic.woff2'
 ];
 
 
