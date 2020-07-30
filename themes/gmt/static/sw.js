68678429008e14900d2738ee440fbf4cc71d7bb4
cferdinandi <chris@gomakethings.com>
2020-07-30 14:18:47 -0400

    Updated service worker

--- themes/gmt/static/sw.js
+++ themes/gmt/static/sw.js
@@ -1,6 +1,6 @@
-/*! GMT Service Worker v2.2.1 | (c) 2020 Chris Ferdinandi | MIT License | http://github.com/cferdinandi/gmt-theme */
+/*! GMT Service Worker v2.2.2 | (c) 2020 Chris Ferdinandi | MIT License | http://github.com/cferdinandi/gmt-theme */
 
-var version = 'gmt_2.2.1';
+var version = 'gmt_2.2.2';
 // Cache IDs
 var coreID = version + '_core';
 var pageID = version + '_pages';
